39921be67cb2c58c32b3a69ed294390d7962c73f
Kizu <demicadelinia.personal@gmail.com>
2026-01-26 21:56:48 -0800

    Added Audio and Camera
    Implemented audio for player footsteps and Cinemachine camera

---
+++ Assets/_1233_StudentWork/Scripts/PlayerAudioHandler.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class PlayerAudioHandler : MonoBehaviour
+{
+    [SerializeField] private AudioSource _footstepSource;
+
+    public void PlayFootstep()
+    {
+        _footstepSource.Play();
+    }
+}
