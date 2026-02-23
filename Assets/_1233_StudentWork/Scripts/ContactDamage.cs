5a6dcfe0b92b1d9b1c2949a361376771c7495617
Kizu <demicadelinia.personal@gmail.com>
2026-02-22 21:22:55 -0800

    Spike Enemy
    Created spike enemy with proper functions

--- Assets/_1233_StudentWork/Scripts/ContactDamage.cs
+++ Assets/_1233_StudentWork/Scripts/ContactDamage.cs
@@ -19,6 +19,8 @@ public class ContactDamage : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
+        Debug.Log("Spike triggered: " + other.name);
+
         TryApplyDamage(other.gameObject);
     }
 
