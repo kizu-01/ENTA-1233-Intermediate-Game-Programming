dbdc004174d31e82f996025e2c17f4f4b8d85492
Kizu <demicadelinia.personal@gmail.com>
2026-04-17 00:47:28 -0700

    Art Mesh and UI Cleanup
    Replaced placeholder meshes in levels and Cleaned up UI for rezisable window function

---
+++ Assets/_1233_StudentWork/Scripts/CameraAspect.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class CameraAspect : MonoBehaviour
+{
+    void Start()
+    {
+        ApplyAspect();
+    }
+
+    void Update()
+    {
+        ApplyAspect(); // update when resizing window
+    }
+
+    void ApplyAspect()
+    {
+        float targetAspect = 16f / 9f;
+        float windowAspect = (float)Screen.width / Screen.height;
+        float scaleHeight = windowAspect / targetAspect;
+
+        Camera cam = GetComponent<Camera>();
+
+        if (scaleHeight < 1.0f)
+        {
+            Rect rect = cam.rect;
+            rect.width = 1.0f;
+            rect.height = scaleHeight;
+            rect.x = 0;
+            rect.y = (1.0f - scaleHeight) / 2.0f;
+            cam.rect = rect;
+        }
+        else
+        {
+            float scaleWidth = 1.0f / scaleHeight;
+
+            Rect rect = cam.rect;
+            rect.width = scaleWidth;
+            rect.height = 1.0f;
+            rect.x = (1.0f - scaleWidth) / 2.0f;
+            rect.y = 0;
+            cam.rect = rect;
+        }
+    }
+}
